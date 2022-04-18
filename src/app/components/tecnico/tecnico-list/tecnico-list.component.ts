9e636797f3c8d4da3be3bb31138443ea2d0e0c62
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2022-04-18 18:55:54 -0300

    Começo da implementação da tabela em TecnicoList

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoListComponent implements OnInit {
 
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Kasino Pereira',
+      cpf: '123.456.789-10',
+      email: 'kasino@email.com',
+      senha: '1234',
+      perfis: ['0'],
+      dataCriacao: '18/04/2022'
+    }
+  ]
+  
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
