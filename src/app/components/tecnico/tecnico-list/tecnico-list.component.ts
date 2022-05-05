1cb8b8cd27a8a13fe21e62524d051be81e1eb5ed
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2022-05-05 07:59:24 -0300

    Aplicando filtro na lista de tecnicos

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -34,4 +34,9 @@ export class TecnicoListComponent implements OnInit {
     })
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 }
