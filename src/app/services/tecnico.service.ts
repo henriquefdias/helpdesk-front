a425b00a109c461591e72b861a85bd85b3144f25
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2022-06-03 08:25:14 -0300

    FindById no tecnico service

--- src/app/services/tecnico.service.ts
+++ src/app/services/tecnico.service.ts
@@ -13,6 +13,10 @@ export class TecnicoService {
     private http: HttpClient
   ) { }
 
+  findById(id: any): Observable<Tecnico> {
+    return this.http.get<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos/${id}`);
+  }
+
   findAll(): Observable<Tecnico[]> {
     return this.http.get<Tecnico[]>(`${API_CONFIG.baseUrl}/tecnicos`);
   }
