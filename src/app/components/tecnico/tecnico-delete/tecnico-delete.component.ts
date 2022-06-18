c0548ebd94b2eeaa4fdef81fea692ccf174343e3
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2022-06-18 11:42:18 -0300

    Update tecnico-delete.component.ts

--- src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
+++ src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tecnico-delete',
@@ -7,9 +12,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoDeleteComponent implements OnInit {
 
-  constructor() { }
+  tecnico: Tecnico = {
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCriacao: ''
+  }
+
+  nome: FormControl = new FormControl(null, Validators.minLength(3));
+  cpf: FormControl = new FormControl(null, Validators.required);
+  email: FormControl = new FormControl(null, Validators.email);
+  senha: FormControl = new FormControl(null, Validators.minLength(3));
+
+  constructor(
+    private service: TecnicoService,
+    private toastr: ToastrService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.tecnico.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+  }
+
+  findById(): void {
+    this.service.findById(this.tecnico.id).subscribe(resposta => {
+      resposta.perfis = [];
+      this.tecnico = resposta;
+    })
+  }
+
+  update(): void {
+    this.service.update(this.tecnico).subscribe(() => {
+      this.toastr.success('Técnico atualizado com sucesso', 'Atualização');
+      this.router.navigate(['tecnicos']);
+    }, ex => {
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          this.toastr.error(element.message);
+        });
+      } else {
+        this.toastr.error(ex.error.message);
+      }
+    })
+  }
+  
+  addPerfil(perfil: any): void {
+    if(this.tecnico.perfis.includes(perfil)) {
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1);
+    } else {
+      this.tecnico.perfis.push(perfil);
+    }
+  }
+
+  validaCampos(): boolean {
+    return this.nome.valid && this.cpf.valid 
+        && this.email.valid && this.senha.valid;
   }
 
 }
