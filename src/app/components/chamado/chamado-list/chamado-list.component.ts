1b107dbe8c0dedf703fe44f9b17d3a5e4bd5928f
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2022-07-06 07:48:54 -0300

    Update chamado-list.component.ts

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Chamado } from 'src/app/models/chamado';
 
 @Component({
   selector: 'app-chamado-list',
@@ -6,6 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chamado-list.component.css']
 })
 export class ChamadoListComponent implements OnInit {
+
+  ELEMENT_DATA: Chamado[] = [
+    {
+      
+    }
+  ];
+  
+  displayedColumns: string[] = ['id', 'titulo', 'cliente', 'dataAbertura', 'prioridade', 'status', 'acoes'];
+  dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor() { }
 
