e8227109d05f98287d6cbf130f698938ed98f28a
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-09-14 08:50:47 -0300

    Update chamado-read.component.ts

--- src/app/components/chamado/chamado-read/chamado-read.component.ts
+++ src/app/components/chamado/chamado-read/chamado-read.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Chamado } from 'src/app/models/chamado';
+import { ChamadoService } from 'src/app/services/chamado.service';
 
 @Component({
   selector: 'app-chamado-read',
@@ -18,30 +22,15 @@ export class ChamadoReadComponent implements OnInit {
     nomeTecnico: ''
   }
 
-  clientes: Cliente[] = [];
-  tecnicos: Tecnico[] = [];
-
-  prioridade: FormControl = new FormControl(null, [Validators.required])
-  status: FormControl = new FormControl(null, [Validators.required])
-  titulo: FormControl = new FormControl(null, [Validators.required])
-  observacoes: FormControl = new FormControl(null, [Validators.required])
-  tecnico: FormControl = new FormControl(null, [Validators.required])
-  cliente: FormControl = new FormControl(null, [Validators.required])
-
   constructor(
     private chamadoService: ChamadoService,
-    private clienteService: ClienteService,
-    private tecnicoService: TecnicoService,
     private toastService: ToastrService,
-    private router: Router,
     private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.chamado.id = this.route.snapshot.paramMap.get('id');
     this.findById();
-    this.findAllClientes();
-    this.findAllTecnicos();
   }
 
   findById(): void {
@@ -50,33 +39,6 @@ export class ChamadoReadComponent implements OnInit {
     }, ex => {
       this.toastService.error(ex.error.error);
     })
-  }
-
-  update(): void {
-    this.chamadoService.update(this.chamado).subscribe(resposta => {
-      this.toastService.success('Chamado atualizado com sucesso', 'Atualizar chamado');
-      this.router.navigate(['chamados']);
-    }, ex => {
-      this.toastService.error(ex.error.error);
-    })
-  }
-
-  findAllClientes(): void {
-    this.clienteService.findAll().subscribe(resposta => {
-      this.clientes = resposta;
-    })
-  }
-
-  findAllTecnicos(): void {
-    this.tecnicoService.findAll().subscribe(resposta => {
-      this.tecnicos = resposta;
-    })
-  }
-
-  validaCampos(): boolean {
-    return this.prioridade.valid && this.status.valid &&
-           this.titulo.valid && this.observacoes.valid &&
-           this.tecnico.valid && this.cliente.valid;
   }
 
   retornaStatus(status: any): string {
